68e764a921520acec3fe29342f71c2c40f62280b
RohanMishra47 <mydearluffy093@gmail.com>
2026-01-30 23:03:22 +0530

    feat: implement DAG validation

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,49 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Body
+from typing import  Dict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {'Ping': 'Pong'}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get('/pipelines/parse')
-def parse_pipeline(pipeline: str = Form(...)):
-    return {'status': 'parsed'}
+def check_is_dag(nodes, edges):
+    # Build adjacency list to know which nodes point to which
+    adj = {node['id']: [] for node in nodes}
+    for edge in edges:
+        adj[edge['source']].append(edge['target'])
+    
+    visited = set()
+    rec_stack = set()
+
+    def has_cycle(v):
+        visited.add(v)
+        rec_stack.add(v)
+        for neighbour in adj.get(v, []):
+            if neighbour not in visited:
+                if has_cycle(neighbour): return True
+            elif neighbour in rec_stack:
+                return True
+        rec_stack.remove(v)
+        return False
+
+    for node in nodes:
+        if node['id'] not in visited:
+            if has_cycle(node['id']): return False # if cycle found, not a DAG
+    return True
+
+@app.post('/pipelines/parse')
+async def parse_pipeline(pipeline: Dict = Body(...)):
+    nodes = pipeline.get('nodes', [])
+    edges = pipeline.get('edges', [])
+    
+    return {
+        "num_nodes": len(nodes),
+        "num_edges": len(edges),
+        "is_dag": check_is_dag(nodes, edges)
+    }
--- frontend/src/submit.js
+++ frontend/src/submit.js
@@ -1,10 +1,35 @@
 // submit.js
 
-export const SubmitButton = () => {
+import { useStore } from "./store";
 
-    return (
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <button type="submit">Submit</button>
-        </div>
+export const SubmitButton = () => {
+  const { nodes, edges } = useStore();
+
+  const handleSubmit = async () => {
+    const response = await fetch("http://127.0.0.1:8000/pipelines/parse", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ nodes, edges }),
+    });
+
+    const data = await response.json();
+
+    alert(
+      `Results:\nNodes: ${data.num_nodes}\nEdges: ${data.num_edges}\nValid DAG: ${data.is_dag}`,
     );
-}
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <button onClick={handleSubmit} type="submit">
+        Submit
+      </button>
+    </div>
+  );
+};
